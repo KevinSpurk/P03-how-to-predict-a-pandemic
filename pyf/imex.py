0aeda3029dd26b0aa5ddacd80d7248444fa69f5a
Kevin Spurk <kevin.lee.spurk@gmx.de>
2021-11-11 18:08:03 +0100

    covid 19 data imports

---
+++ pyf/imex.py
@@ -0,0 +1,20 @@
+# functions handling operations with additional files  
+
+import pandas as pd
+import numpy as np
+import datetime
+import math
+import random
+import re
+
+
+
+# function to export df to csv with var naming
+# inputs: df, stings for constant and variable part of file name
+# outputs: df
+def df_to_csv_format(df, name_cst, name_var, index=False):
+    file_name = name_cst + '_' + name_var
+    df.to_csv(f'{file_name}.csv', sep=',', index=index)
+    return df
+
+
