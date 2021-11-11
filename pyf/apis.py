ea5723f4038825846fa9eb2cff29b6617d1d45ba
Kevin Spurk <kevin.lee.spurk@gmx.de>
2021-11-11 19:32:34 +0100

    fb data imports, structure

--- pyf/apis.py
+++ pyf/apis.py
@@ -11,6 +11,7 @@ from translate import Translator
 from pytrends.request import TrendReq
 from pytrends import dailydata
 from datetime import date, timedelta
+from imex import df_to_csv_format
 
 
 # DeepL
@@ -46,4 +47,104 @@ def deepl_translate_list(auth, keywords, lang_out, lang_in='en'):
     df_keywords_translated = pd.DataFrame(keywords_translated)
             
     return keywords_translated, df_keywords_translated
+
+
+
+# Google
+
+# get the search trend data for a series of keywords in multiple languages
+# inputs: dict with 2-char id of starget countries as keys and list of keywords in target language as values ('GB' has to be the first lang),
+# start and end date in format YYYY-MM, sleep timer in sec
+# outputs: dict with search trend results, creates csv files of results for each language
+def g_trends_multilang(kw_dict, dt_start, dt_end, sleep_time=60):
+    # df for all results
+    trends_all = pd.DataFrame({})
+    
+    # loop through lang
+    for geo in kw_dict.keys():
+        # df for results of one loc
+        search_trend = pd.DataFrame({})
+        
+        # loop through keywords
+        for k in kw_dict[geo]:
+            # get df with search trend for kw
+            k_trend = dailydata.get_daily_data(k, int(dt_start.split('-')[0]), int(dt_start.split('-')[1]), int(dt_end.split('-')[0]), int(dt_end.split('-')[1]), geo=geo)
+            time.sleep(sleep_time)
+            
+            # transform df
+            k_trend = g_trends_df_transform(k_trend)
+            
+            # add missing dates
+            k_trend_completed = g_trends_complete_timeframe(df=k_trend, kw=k)
+            
+            # add df with search trend for kw to df for results of one loc
+            search_trend['date'] = k_trend_completed['date']
+            search_trend[k] = k_trend_completed[k]
+        
+        # add loc to df 
+        search_trend['location'] = geo
+        # export df to csv
+        search_details = dt_start + '_' + dt_end + '_' + geo
+        df_to_csv_format(df=search_trend, name_cst='g_trend', name_var=search_details, index=False)
+        
+        # concat df with df holding all results
+        if len(trends_all) > 0:
+            search_trend.columns = trends_all.columns
+        trends_all = pd.concat([trends_all, search_trend], axis=0)
+    
+    trends_all.reset_index(inplace=True)
+    trends_all = trends_all.sort_values(by=['date'], ignore_index=True)
+    
+    return trends_all
+
+
+# create data pionts in df for dates where search trend data is missing, sub func of g_trends_multilang
+# inputs: df of search trend values for one keyword in one language, string of keyword
+# outputs: df of search trend values with added NaN for missing dates 
+def g_trends_complete_timeframe(df, kw):
+    # set start and end date for loop
+    dt_s = df['date'][0]
+    pos_end = len(df) - 1
+    dt_e = df['date'][pos_end]
+    
+    # lists for missing days
+    dates_missing = []
+    trend_missing = []
+    
+    # loop to find missing dates in df
+    while dt_s <= dt_e:
+        date_check = False
+        for d in df['date']:
+            if d == dt_s:
+                date_check = True
+        # append missing date to lists
+        if date_check == False:
+            dates_missing.append(dt_s)
+            trend_missing.append(np.NaN)
+        dt_s += timedelta(days=1)
+    
+    # df for missing dates
+    df_dates_missing = pd.DataFrame({'date': dates_missing, kw: trend_missing})
+    df_completed = pd.concat([df, df_dates_missing], axis=0)
+    df_completed['date'] = pd.to_datetime(df_completed['date'], errors='coerce')
+    df_completed = df_completed.sort_values(by=['date'], ignore_index=True)
+    
+    return df_completed   
+
+
+# transform the search trend fetch into a usable df, sub func of g_trends_multilang
+# inputs: df with results from 'get_daily_data'
+# outputs: df reformated 
+def g_trends_df_transform(df):
+    # drop unwanted col
+    df = df.drop(df.iloc[:, 1:], axis=1)
+            
+    # get date as col
+    df.reset_index(inplace=True)
+    df.columns = df.columns.str.replace('_unscaled', '')
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df['date'] = df['date'].dt.date
+    return df
+
+
 
