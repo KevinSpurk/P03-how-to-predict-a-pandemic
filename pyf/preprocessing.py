42fade3f8d5eeeb59683e5eef04d78d7b65cd550
Kevin Spurk <kevin.lee.spurk@gmx.de>
2021-11-26 00:48:27 +0100

    encoding cat, train test split, model tests

--- pyf/preprocessing.py
+++ pyf/preprocessing.py
@@ -567,6 +567,31 @@ def timeseries_clustered_target_lags(df, timeframe, cluster_by, target, min_lag,
     return results
 
 
+# encoding categorical col with get dummies
+# inputs: df, in_columns=col to encode (list, default=all object col), skip_columns=col not to encode (list, default=empty), 
+# drop_first= value for pd.get_dummies
+# outputs: df
+def encoding_get_dummies(df, in_columns=[], skip_columns=[], drop_first=True):
+    # set col to all object col by default
+    if in_columns == []:
+        in_columns = list(df.select_dtypes(np.object).columns)
+        # list col not to include in encoding
+        for col in skip_columns:
+            in_columns.remove(col)
+            
+    # split df into df to encode and df with other col    
+    df_encode = df[in_columns]
+    df_rest = df.drop(in_columns, axis=1)
+    
+    # encode
+    df_encode = pd.get_dummies(df_encode, drop_first=drop_first)
+    
+    # concat encoded and other col
+    df = pd.concat([df_encode, df_rest], axis=1)
+    
+    return df
+
+
 # function to drop columns
 # inputs: df, list of columns
 # outputs: df
